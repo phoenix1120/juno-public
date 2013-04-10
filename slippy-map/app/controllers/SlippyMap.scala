2147704f3bfb072efa15366cb7ea6da827c8699f
Phoenix <phoenix@phoenix-Lenovo-IdeaPad-Y470.(none)>
2013-04-10 11:07:38 -0700

    made a small little change that has a custom tile showing for every tile on the slippy map

--- slippy-map/app/controllers/SlippyMap.scala
+++ slippy-map/app/controllers/SlippyMap.scala
@@ -4,6 +4,7 @@ import play.api.mvc.{Controller, Action}
 import junoshared.web.WebRequest
 import play.api.libs.json.{Json, JsValue}
 import play.api.libs.json.Json._
+import scala.math._
 
 
 object SlippyMap extends Controller {
@@ -50,4 +51,29 @@ object SlippyMap extends Controller {
     val postJson = Map("helloWorld" -> "this is my hello world value")
     WebRequest.postJson(postHwUrl, Json.toJson(postJson))
   }
+
+  /*
+  * This is the code that calculates the tiles needed
+  * */
+  case class Tile(x: Int,y: Int, z: Short){
+    def toLatLon = new LatLonPoint(
+      toDegrees(atan(sinh(Pi * (1.0 - 2.0 * y.toDouble / (1<<z))))),
+      x.toDouble / (1<<z) * 360.0 - 180.0,
+      z)
+    def toURI = new java.net.URI("http://tile.openstreetmap.org/"+z+"/"+x+"/"+y+".png")
+  }
+
+  case class LatLonPoint(lat: Double, lon: Double, z: Short){
+    def toTile = new Tile(
+      ((lon + 180.0) / 360.0 * (1<<z)).toInt,
+      ((1 - log(tan(toRadians(lat)) + 1 / cos(toRadians(lat))) / Pi) / 2.0 * (1<<z)).toInt,
+      z)
+  }
+
+  //Usage:
+  val point = LatLonPoint(51.51202,0.02435,17)
+  val tile = point.toTile
+  // ==> Tile(65544,43582,17)
+  val uri = tile.toURI
+  // ==> http://tile.openstreetmap.org/17/65544/43582.png
 }
